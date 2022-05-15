deabc8d0cdf56ab8b96fa5d0edaca4579b7f613f
Burak_Fidan <103516974+FBurak@users.noreply.github.com>
2022-05-15 12:50:03 +0300

    Update log1.php

--- log1.php
+++ log1.php
@@ -1,11 +1,7 @@
 
 
-<!----------------------------------------------------------------------------------------------------------------------------------------->
- <br><br><br><br><br><br><br> 
 	
-   <div class="container py-5" style="color:white; text-align:center; align-items:center;">
-    <div class="row">
-      <div class="col loginMesaj">
+   
         <?php if($_POST["ADI"]=="b201210081@sakarya.edu.tr" && $_POST["PAROLA"]=="b201210081")
 			{
         echo"Hoşgeldiniz ".$_POST["ADI"];
@@ -16,9 +12,4 @@
         header("refresh:3; login.html");
                  }    
         ?>
-        </div>
-      </div>
-    </div> 
-   
-<br><br><br><br><br><br><br><br><br>
-<!---------------------------------------------------------------------------------------------------------------------------------------->
+      
