f06d3016e2732214a98ca5358c3bd428d4480748
Burak_Fidan <103516974+FBurak@users.noreply.github.com>
2022-05-15 16:57:45 +0300

    Add files via upload

---
+++ iletişim.php
@@ -0,0 +1,30 @@
+	<table>
+			<tr>
+				<th scope="row">İsim:</th>
+				<td><?php echo $_POST["adı"]?></td>
+			</tr>
+			<tr>
+				<th scope="row">Soyisim:</th>
+				<td><?php echo $_POST["soyadı"]?></td>
+			</tr>
+			<tr>
+				<th scope="row">Email:</th>
+				<td><?php echo $_POST["email"]?></td>
+			</tr>
+			<tr>
+				<th scope="row">Cinsiyet:</th>
+				<td><?php echo $_POST["cinsiyet"]?></td>
+			</tr>
+			<tr>
+				<th scope="row">Hesaplar</th>
+				<td><?php echo $_POST["sm1"]?></td>
+			</tr> 
+			<tr>
+			<th scope="row">öğrencino:</th>
+				<td><?php echo $_POST["öğrencino"]?></td>
+			</tr>
+      		<tr>
+			  <th scope="row">Adres:</th>
+				<td><?php echo $_POST["adres"]?></td>
+			</tr>
+	</table>
