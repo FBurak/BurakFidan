3ad80b038eafa50ffd2758a5ba18260e2a50ca9e
Burak_Fidan <103516974+FBurak@users.noreply.github.com>
2022-05-15 12:31:45 +0300

    login sayfası php

---
+++ log1.php
@@ -0,0 +1,24 @@
+
+
+<!----------------------------------------------------------------------------------------------------------------------------------------->
+ <br><br><br><br><br><br><br> 
+	
+   <div class="container py-5" style="color:white; text-align:center; align-items:center;">
+    <div class="row">
+      <div class="col loginMesaj">
+        <?php if($_POST["ADI"]=="nafiz.fidan@sakarya.edu.tr" && $_POST["PAROLA"]=="b201210081")
+			{
+        echo"Hoşgeldiniz ".$_POST["ADI"];
+        echo"<br>Girişiniz onaylandı.";
+            }
+             else{
+        echo "Girdiğiniz kullanıcı adı veya şifre hatalı.";
+        header("refresh:3; login.html");
+                 }    
+        ?>
+        </div>
+      </div>
+    </div> 
+   
+<br><br><br><br><br><br><br><br><br>
+<!---------------------------------------------------------------------------------------------------------------------------------------->
