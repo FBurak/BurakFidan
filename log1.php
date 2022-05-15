bbc553b370fb46bae6aa41ff157c991fd5a9c038
Burak_Fidan <103516974+FBurak@users.noreply.github.com>
2022-05-15 12:35:39 +0300

    Update log1.php

--- log1.php
+++ log1.php
@@ -6,7 +6,7 @@
    <div class="container py-5" style="color:white; text-align:center; align-items:center;">
     <div class="row">
       <div class="col loginMesaj">
-        <?php if($_POST["ADI"]=="nafiz.fidan@sakarya.edu.tr" && $_POST["PAROLA"]=="b201210081")
+        <?php if($_POST["ADI"]=="b201210081@sakarya.edu.tr" && $_POST["PAROLA"]=="b201210081")
 			{
         echo"Hoşgeldiniz ".$_POST["ADI"];
         echo"<br>Girişiniz onaylandı.";
